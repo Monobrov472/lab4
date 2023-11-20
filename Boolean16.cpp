9fe6b048e947fcc9ba35b2af32aec84fc8c3a2ae
Monobrov472 <144647756+Monobrov472@users.noreply.github.com>
2023-11-20 17:02:52 +0200

    Add files via upload

---
+++ Boolean16.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    // Введення числа
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    // Перевірка чи число є парним двозначним
+    bool is_even_two_digit = (number % 2 == 0) && (number >= 10 && number <= 99);
+
+    // Виведення результату
+    if (is_even_two_digit) {
+        cout << "This number is an even two-digit number." << endl;
+    }
+    else {
+        cout << "This number is not an even two-digit number." << endl;
+    }
+
+    return 0;
+}
